faeb7d96a8d7dc803701f0924d88a104cb0860e2
Damien Oliver <damien.oliver01@student.wandw.ac.nz>
2026-05-25 13:23:13 +1200

    Starting of menu

--- Program.cs
+++ Program.cs
@@ -3,12 +3,22 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Does this work?");
 
-            Console.WriteLine("testing again");
-            Console.WriteLine("Damien test 3");
+        {
+            //declaring varaibles
+            int choice;
+
+
+            Console.WriteLine("\t******Welcome to the Expense App*******");
+            Console.WriteLine("\t Login or Register for an account");
+
+            Console.WriteLine("1.Login");
+            Console.WriteLine("2. Register");
+            Console.WriteLine("3. Administrator Login");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("Please select a choice");
+            choice = Convert.ToInt32(Console.ReadLine());
+            
         }
     }
 }
