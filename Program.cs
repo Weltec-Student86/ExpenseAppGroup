16c3629f0c7602a05a12374b3133abe0477a335c
kitku-png <katie.sampson01@student.wandw.ac.nz>
2026-05-25 13:55:09 +1200

    Creating menus for login and home

--- Program.cs
+++ Program.cs
@@ -8,17 +8,181 @@
             //declaring varaibles
             int choice;
 
+            //do { }
 
             Console.WriteLine("\t******Welcome to the Expense App*******");
             Console.WriteLine("\t Login or Register for an account");
 
-            Console.WriteLine("1.Login");
+            Console.WriteLine("1. Login");
             Console.WriteLine("2. Register");
             Console.WriteLine("3. Administrator Login");
-            Console.WriteLine("4. Exit");
+            Console.WriteLine("99. Exit");
             Console.WriteLine("Please select a choice");
             choice = Convert.ToInt32(Console.ReadLine());
-            
-        }
-    }
-}
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Login");
+                    Console.WriteLine();
+                    UserLogin();
+                    break;
+
+                case 2:
+                    Console.WriteLine("Register");
+                    Console.WriteLine();
+                    Register();
+                    break;
+
+                case 3:
+                    Console.WriteLine("Admin login");
+                    AdminLogin();
+                    break;
+
+                case 4:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Enter a valid option");
+                    break;
+            }
+
+            //}
+            //  while ()
+
+        }//end of main-----------------------------------------------------------------------------------------------------------------------------------
+
+        //login method for user
+
+        public static void UserLogin()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Enter your username:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+
+            if (userName == "user" && password == "password")
+            {
+                Console.WriteLine("Login success");
+            }
+            else
+            {
+                Console.WriteLine("Wrong login");
+            }
+
+            UserHome();
+
+        }//end of Login method
+
+        public static void Register()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Enter your first name:");
+            string firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name:");
+            string lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter your date of birth:");
+            string dob = Console.ReadLine();
+
+            Console.WriteLine("Enter your username:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your password:");
+            string password = Console.ReadLine();
+
+            Console.WriteLine("Re-enter password:");
+            string reEnteredPassword = Console.ReadLine();
+
+            if (password == reEnteredPassword)
+            {
+                Console.WriteLine("Success");
+            }
+            else
+            {
+                Console.WriteLine("Passwords do not match");
+            }
+
+        }//end of Register method
+
+        public static void AdminLogin()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Enter your admin username:");
+            string adminUserName = Console.ReadLine();
+
+            Console.WriteLine("Enter your admin password:");
+            string adminPassword = Console.ReadLine();
+
+            if (adminUserName == "admin" && adminPassword == "password")
+            {
+                Console.WriteLine("Login success");
+            }
+            else
+            {
+                Console.WriteLine("Wrong login");
+            }
+        }//end of Login method
+
+        public static void UserHome()
+        {
+            Console.Clear();
+
+            Console.WriteLine("1. View expenses");
+            Console.WriteLine("2. Add new expense");
+            Console.WriteLine("3. Update existing expense");
+            Console.WriteLine("4. Remove expense");
+            Console.WriteLine("5. View savings");
+            Console.WriteLine("99. Exit");
+
+            Console.WriteLine("Please select an option:");
+            int homeChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (homeChoice)
+            {
+                case 1:
+                    Console.WriteLine("Viewing expenses");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Add new expense");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Update existing expense");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Remove expense");
+                    break;
+
+                case 5:
+                    Console.WriteLine("View savings");
+                    break;
+
+                case 99:
+                    Environment.Exit(0);
+                    break;
+
+                default:
+                    Console.WriteLine("Please select a valid option.");
+                    break;
+
+
+            }//end of switch
+        
+        }//end of UserHome
+
+
+
+
+    }//end of class program---------------------------------------------------------------------------------------------------------------------------------------------------
+
+}//end of namespace------------------------------------------------------------------------------------------------------------------------------
