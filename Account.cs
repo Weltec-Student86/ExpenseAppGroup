74a6b0e561008c15c21bfc5a0185497f28483ce4
kitku-png <katie.sampson01@student.wandw.ac.nz>
2026-05-26 18:21:40 +1200

    Adding class structure

---
+++ Account.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpenseAppGroup
+{
+    public abstract class Account
+    {
+        private string firstName;
+        private string lastName;
+        private DateTime birthday = new DateTime();
+        private string username;
+        private string password;
+
+    }//end of account
+}
